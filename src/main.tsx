b4e0c67794bac2566b5a8e4397ec83a5f34a573d
imnotmanya <sultanovdaniyar80@gmail.com>
2026-05-01 00:58:00 +0500

    fix(pwa): force unregister service workers to prevent POST request interception

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Force unregister all service workers to clear any sticky cache issues
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
