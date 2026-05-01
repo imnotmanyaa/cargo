29743c2acf063da8b0fac90fbf1eeb13ea66d022
imnotmanya <sultanovdaniyar80@gmail.com>
2026-05-01 16:38:03 +0500

    revert temp wipe db

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
@@ -22,24 +21,6 @@ func main() {
 
 	if err := db.Migrate(); err != nil {
 		log.Fatalf("migrate postgres: %v", err)
-	}
-
-	log.Println("STARTING WIPE OF DATABASE EXCEPT SPECIFIED USERS...")
-	_, errWipe := db.Pool().Exec(context.Background(), `
-		TRUNCATE wagon_shipments, wagons, shipments, shipment_history, notifications, audit_log, frequent_clients CASCADE;
-		DELETE FROM users WHERE lower(btrim(email)) NOT IN (
-			'co@cargo.kz',
-			'almatapriem@cargo.kz',
-			'admin@admin.com',
-			'astanapriem@cargo.kz',
-			'astana@cargo.kz',
-			'admin@cargo.kz'
-		);
-	`)
-	if errWipe != nil {
-		log.Printf("wipe error: %v", errWipe)
-	} else {
-		log.Println("WIPED DATABASE SUCCESSFULLY!")
 	}
 
 	repo := postgres.NewRepository(db.Pool())
